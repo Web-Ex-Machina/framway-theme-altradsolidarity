3a9e23d8eb6aaadc2b3f11a7e1cc20f2923b93c6
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-09-05 11:15:14 +0200

    styles, scripts, config

--- altradsolidarity.js
+++ altradsolidarity.js
@@ -1,3 +1,19 @@
-$(function(){
-
-});
+document.querySelector('#footer').classList.add('v2')
+/**
+ * switch the header between reduced and not-reduced states
+ * @param  {Boolean} reduce 
+ */
+app.HeaderFW.prototype.navSwitcher = function(reduce = false){
+    var header = this;
+    if (reduce) {
+        header.$el.addClass('is-reduce');
+        if (header.$topbar) header.$topbar.appendTo(header.navPanelMenus.root.$el.find('.panel__actions'));
+        if (header.$search) header.$search.appendTo(header.navPanelMenus.root.$el.find('.panel__actions'));
+        if (header.$lang)   header.$lang.appendTo(header.navPanelMenus.root.$el.find('.panel__actions'));
+    } else {
+        header.$el.removeClass('is-reduce');
+        if (header.$topbar) header.$nav.append(header.$topbar);
+        if (header.$search) header.$nav.append(header.$search);
+        if (header.$lang)   header.$nav.append(header.$lang);
+    }
+};
--- config.js
+++ config.js
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'solidarity': '#B9A996',
+	},
+	'primary' : 'colors(solidarity)',
+	"body":{
+		"font-color": "colors(greystrong)",
+		'line-height' 	: '1.25',
+	},
+	"btn" :{
+		"radius": "50em",
+		"padding-x": "1.5em",
+		"font-color": "colors(white)",
+		"border-size": "1px",
+	},
+	"input":{
+		"radius": "30px",
+	}
+};
